69557cf8651d1dd56d450d4d4271b3295d30f590
Raquel Nascimento <raquel.fm.nascimento@gmail.com>
2020-11-13 21:03:00 +0000

    test

--- src/components/LayoutElements/Review.js
+++ src/components/LayoutElements/Review.js
@@ -21,7 +21,6 @@ class Review extends Component {
 
   fetchDataUser = () => {
     const { id } = this.state;
-    return console.log("id review user", id);
     getAllUserReview(id)
       .then((response) => {
         console.log("response list review user", response);

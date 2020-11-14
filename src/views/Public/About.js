d03e30d271912518c681b5a6e521e131c852b1d1
Raquel Nascimento <raquel.fm.nascimento@gmail.com>
2020-11-14 11:57:48 +0000

    about page

--- src/views/Public/About.js
+++ src/views/Public/About.js
@@ -7,21 +7,19 @@ class About extends React.Component {
   render() {
     return (
       <div className="view text-center p-3 p-md-5 m-md-3">
-        <h2>About page</h2>
+        <h2>
+          <strong>CLICK-APP</strong>
+        </h2>
         <p>
-          Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-          accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae
-          ab illo inventore veritatis et quasi architecto beatae vitae dicta
-          sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-          aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos
-          qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui
-          dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
-          quia non numquam eius modi tempora incidunt ut labore et dolore magnam
-          aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum
-          exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex
-          ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in
-          ea voluptate velit esse quam nihil molestiae consequatur, vel illum
-          qui dolor
+          The application is born to bring local academic professionals closer
+          to families in need.
+          <br />
+          Covering the necessity of supporting our children. <br />
+          Facilitating the reconciliation of work and family life. Reliable and
+          secure for all users.
+          <br />
+          Created with 💜 fully remotly between LISBON & MADRID and designed for
+          everyone without exception. <br />
         </p>
         <br />
         <div className="col-lg-6 col-md-6 mb-lg-0 mb-5">
@@ -64,7 +62,7 @@ class About extends React.Component {
             <img src="/logosanti.png" width="30" height="30" alt="logosanti" />
           </a>
         </p>
-        <p>Vídeo de Pavel Danilyuk en Pexels</p>
+        <p>Vídeo from Pavel Danilyuk in Pexels</p>
       </div>
     );
   }

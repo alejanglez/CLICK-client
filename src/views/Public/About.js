881a199696824549a6487729c2da345d1e0f5e6f
Raquel Nascimento <raquel.fm.nascimento@gmail.com>
2020-11-14 10:42:35 +0000

    accepted serv corrected

--- src/views/Public/About.js
+++ src/views/Public/About.js
@@ -1,11 +1,9 @@
 import React from "react";
-
 class About extends React.Component {
   state = {
     authenticated: this.props.authenticated,
     role: this.props.role,
   };
-
   render() {
     return (
       <div className="view text-center p-3 p-md-5 m-md-3">
@@ -26,6 +24,40 @@ class About extends React.Component {
           qui dolor
         </p>
         <br />
+        <div className="col-lg-6 col-md-6 mb-lg-0 mb-5">
+          <div className="avatar mx-auto">
+            <img
+              src="./profile1.jpg"
+              width="200"
+              height="200"
+              className="rounded-circle z-depth-1"
+              alt="Sample avatar"
+            />
+          </div>
+          <h5 className="font-weight-bold mt-4 mb-3">Raquel Nascimento</h5>
+          <p className="text-uppercase blue-text">
+            <strong>CTO</strong>
+          </p>
+          <p className="grey-text">
+            Fullstack Dev. 💻 & Jiu-jitsu passionate🥋
+          </p>
+        </div>
+        <div className="col-lg-6 col-md-6 mb-lg-0 mb-5">
+          <div className="avatar mx-auto">
+            <img
+              src="./profile2.jpg"
+              className="rounded-circle z-depth-1"
+              width="200"
+              height="200"
+              alt="Sample avatar"
+            />
+          </div>
+          <h5 className="font-weight-bold mt-4 mb-3">Alejandro González</h5>
+          <p className="text-uppercase blue-text">
+            <strong>CTO</strong>
+          </p>
+          <p className="grey-text">Fullstack Dev. 💻 & Swimming lover 🏊</p>
+        </div>
         <p>
           Typography "Britanica" by SANTIREYDESIGN
           <a href="http://santireydesign.com/">
@@ -37,5 +69,4 @@ class About extends React.Component {
     );
   }
 }
-
 export default About;
